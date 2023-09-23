5346d903b884c96dbe325df43e5bdf588648ece2
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-09-23 08:58:02 +0530

    python file

--- Numberpyramid.py
+++ Numberpyramid.py
@@ -83,9 +83,60 @@
 #         print(j*2-1,end=" ")
 #     print(" ")
 # --------------------------------------------------------------------------------------
-a=int(input("Enter the limit"))
-for i in range (1,a+1)
-    for p in range(a):
-        for j in range(1,i+1):
-            print(j*2,end=" ")
-    print(" ")
+# 2
+# 4 6 8 
+# 10 12 14 16 18
+# 20 22 24 26 28 30 32
+
+# a=int(input("Enter the limit"))
+# col=1
+# n=2
+# for i in range (1,a+1):
+#     for j in range(1,col+1):
+#         print(n,end=" ")
+#         n=n+2
+#     col=col+2
+#     print(" ")
+# -------------------------------------------------------------------------------------------
+
+# 1
+# 3 5 7
+# 9 11 13 15 17
+# 19 21 23 25 27 29 31
+
+# a=int(input("Enter the limit"))
+# col=1
+# n=2
+# for i in range (1,a+1):
+#     for j in range(1,col+1):
+#         print(n-1,end=" ")
+#         n=n+2
+#     col=col+2
+#     print(" ")
+# ---------------------------------------------------------------------------------------------------
+# 1
+# 1 2
+# 1 2 1
+# 1 2 1 2
+
+# a=int(input("Enter the limit"))
+# for i in range (1,a+1):
+#     for j in range(1,i+1):
+#         if j%2==0:
+#             print("2",end=" ")
+#         else:
+#             print("1",end=" ")
+#     print(" ")
+
+    # ---------------------------------------------------------------------------------------------------------
+# 1
+# 2 1 
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
+
+# a=int(input("Enter the limit"))
+# for i in range (1,a+1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print(" ")
