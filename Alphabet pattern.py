2219a5a678724a09c6e1bed2c57ad958225a2c58
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-09-25 09:00:14 +0530

    Python file

---
+++ Alphabet pattern.py
@@ -0,0 +1,72 @@
+# A
+# B C
+# D E F
+
+# a=int(input("Enter the limit"))
+# alphbet=65
+# for i in range (1,a+1):
+#     for j in range(1,i+1):
+#         alpha=chr(alphbet)
+#         print(alpha,end=" ")
+#         alphbet=alphbet+1
+#     print(" ")
+# ----------------------------------------------------------------------------------------------------
+# A
+# A B 
+# A B C 
+# A B C D
+
+# a=int(input("Enter the limit"))
+# alphbet=65
+# for i in range (0,a+1):
+#     for j in range(0,i+1):
+#         print(chr(j+alphbet),end=" ")
+#     print(" ")
+# -----------------------------------------------------------------------------------------------------------
+# A 
+# B B
+# C C C
+# D D D D
+# a=int(input("Enter the limit"))
+# alphbet=65
+# for i in range(0,a):
+#     for j in range(0,i+1):
+#         print(chr(i+alphbet),end=" ")
+#     print(" ")
+# ---------------------------------------------------------------------------------------------------------
+# A 
+# A B C 
+# A B C D E
+# a=int(input("Enter the limit "))
+# col=1
+# n=64
+# for i in range (1,a+1):
+#     for j in range(1,col+1):
+#         print(chr(j+n),end=" ")
+#     col=col+2
+#     print(" ")
+# --------------------------------------------------------------------------------------------------------------
+# A 
+# B A
+# C B A 
+# a=int(input("Enter the limit "))
+# alphbet=64
+# for i in range (1,a+1):
+#     for j in range(i,0,-1):
+#         print(chr(j+alphbet),end=" ")
+#     print(" ")
+# ----------------------------------------------------------------------------
+# A 
+# B C D 
+# E F G H I 
+# a=int(input("Enter the limit "))
+# col=1
+# n=65
+# for i in range (1,a+1):
+#     for j in range(1,col+1):
+#         alpha=chr(n)
+#         print(alpha,end=" ")
+#         n=n+1
+#     col=col+2
+#     print(" ")
+# ------------------------------------------------------------------------------------
---
+++ Star pattern.py
@@ -0,0 +1,25 @@
+# *
+# * 1 *
+# * 1 2 *
+# * 1 2 3 *
+# * 1 2 *
+# * 1 *
+# *
+a=int(input("Enter the limit"))
+for i in range (a):
+    for j in range(i):
+        if j%2==
+        print(i,end=" ")
+    print(" ")
+for i in range(a,0,-1):
+    for j in range(i):
+        print("*", end=" ")    
+    print()
+
+
+    #    for j in range(1,i+1):
+#         if j%2==0:
+#             print("2",end=" ")
+#         else:
+#             print("1",end=" ")
+#     print(" ")
