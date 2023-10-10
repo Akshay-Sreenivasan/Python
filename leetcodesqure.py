3dd0759a9f8bfb8ae259419c38acb01be3dfd9e7
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-10-10 08:49:45 +0530

    Python

---
+++ leetcodesqure.py
@@ -0,0 +1,18 @@
+# -*********************************************leetcode@akshay123****************************************
+num=int(input('Enter the integer '))
+def square(x):
+    if (x==0):
+        return 0
+    start=1
+    end=x/2+1
+    while(start<=end):
+        mid=start+(end-start)/2
+        if(mid==x/mid):
+            return mid
+        elif(mid<x/mid):
+            start=mid+1
+        else:
+            end=mid-1
+        return end
+    
+print('Square root of',num,'is',square(num))
