8eae3a9e0a14227eafa42d0372c9f311ed1a1a9f
Akshay-Sreenivasan <135939039+Akshay-Sreenivasan@users.noreply.github.com>
2023-11-09 09:00:41 +0530

    python

--- Factorial calculator.py
+++ Factorial calculator.py
@@ -1,21 +1,21 @@
 from tkinter import *
-import math
 w=Tk()
 w.config(background='#E6FFFF')
 w.geometry('300x300')
-l1=Label(w,text='Length')
+l1=Label(w,text='Number')
 e1=Entry(w,width=20)
-l2=Label(w,text='Height')
-e2=Entry(w,width=20)
 l1.grid(row=0,column=0)
-l2.grid(row=1,column=0)
 e1.grid(row=0,column=1,padx=10)
-e2.grid(row=1,column=1,padx=10)
-def area():
-    r=(int(e1.get())*int(e2.get()))
-    e3.delete(0,END)
-    e3.insert(END,r)
-b1=Button(w,text='SUBMIT',command=area).grid(row=3,column=0)
+def fact():
+    if int(e1.get()) == 0:
+        return 1
+    else:
+        factorial=1
+        for i in range(1,int(e1.get())+1):
+            factorial*=i
+        e3.delete(0,END)
+        e3.insert(END,factorial)
+b1=Button(w,text='Calculate',command=fact).grid(row=3,column=0)
 e3=Entry(w,width=20)
 e3.grid(row=4,column=1)
 w.mainloop()
